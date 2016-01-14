81111f05973e3bdcd1208fa14341597fc49cdf9a
zhi.zhou <zhi.zhou@chaitin.com>
2016-01-14 15:02:23 +0800

    REPL with color

--- cli.js
+++ cli.js
@@ -2,47 +2,40 @@
 
 'use strict';
 
-var fs = require('fs');
-
-var deobfuscator = require('./lib/deobfuscator');
+const fs = require('fs');
+const colors = require('colors');
+const deobfuscator = require('./');
 
 // run as a command line tool
 //
 // Usage:
 //   node deobfuscator.js input [output]
 // 
-if (typeof process !== 'undefined') {
-  let argv = process.argv;
 
-  if ([3, 4].indexOf(argv.length) > -1) {
-    let src = argv[2];
-    let dst = argv[3];
+if ([3, 4].indexOf(process.argv.length) > -1) {
+  let src = process.argv[2];
+  let dst = process.argv[3];
 
-    fs.readFile(src, function(err, content) {
-      if (err) {
-        throw err;
-      }
+  let code = fs.readFileSync(src).toString('utf8');
+  if (dst)
+    fs.writeFile(dst, code);
+  else
+    console.log(code.green);
 
-      var code = deobfuscator.clean(content.toString('utf8'));
-      if (dst) {
-        fs.writeFile(dst, code);
-      } else {
-        console.log(code);
-      }
-    });
-  } else if (argv.length === 2) {
-    process.stdin.setEncoding('utf8');
-    process.stdin.on('readable', function() {
-      var chunk = process.stdin.read();
-      if (chunk && chunk.length) {
-        try {
-          console.log('----'.repeat(20));
-          console.log(deobfuscator.clean(chunk));
-        } catch (ex) {
-          console.log(`Error: ${ex.description} at line ${ex.lineNumber}, col ${ex.column}`);
+} else if (process.argv.length === 2) {
+  process.stdin.setEncoding('utf8');
+  process.stdin.on('readable', function() {
+    var chunk = process.stdin.read();
+    if (chunk && chunk.length) {
+      try {
+        console.log(deobfuscator.clean(chunk).green);
+      } catch (ex) {
+        if (ex.lineNumber && ex.column) {
+          console.log(`Error: ${ex.description} at line ${ex.lineNumber}, col ${ex.column}`.red);  
         }
       }
-      process.stdout.write('etacsufbo > ');
-    });
-  }
+    }
+    process.stdout.write('> ');
+  });
 }
+
