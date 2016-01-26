deb5060b448253ed12f420f4ebf8c4c0799c9fa3
zhi.zhou <zhi.zhou@chaitin.com>
2016-01-26 22:15:46 +0800

    simplifier api

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const escodegen = require('escodegen');
+const esprima = require('esprima');
 const simplifier = require('./lib/simplifier.js');
 
-exports.clean = exports.simplify = simplifier.simplify;
+exports.simplify = simplifier.simplify;
+exports.clean = code => escodegen.generate(simplifier.simplify(esprima.parse(code)));
